152f848393bfdf9250bce8fdc8b95e4492d56c5c
Hrishikesh S Nair <125496407+Hrishikesh-S-Nair@users.noreply.github.com>
2023-08-26 23:17:43 +0530

    fg

--- C++/Essentials of C and C++ Concepts/Parameter_Passing.cpp
+++ C++/Essentials of C and C++ Concepts/Parameter_Passing.cpp
@@ -1,9 +1,24 @@
+//Call By Address
 #include <iostream>
 
 using namespace std;
 
+void swap(int *a, int *b)
+{
+    int c;
+    c=*a;
+    *a=*b;
+    *b=c; 
+}
+
 int main()
 {
+    int num1=10, num2=15;
+
+    swap(&num1, &num2);
     
-   
+    cout<<"First Number "<<num1<<endl;
+    cout<<"Second Number "<<num2<<endl; 
+    
+    return 0;
 }
